13db6c1dcf23210c2f31a10e7fe158659297bf0c
Frank <119357315+frankxhunter@users.noreply.github.com>
2025-05-04 13:37:55 +0200

    Arreglando la subida anterior

--- src/main/java/com/shortify/Main.java
+++
@@ -1,7 +0,0 @@
-package com.shortify;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/com/shortify/utils/UserRegisterException.java
+++
@@ -1,7 +0,0 @@
-package com.shortify.utils;
-
-public class UserRegisterException extends Exception {
-    public UserRegisterException(String msg){
-        super(msg);
-    }
-}
