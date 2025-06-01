6053ac3eb66db96bfab380ce3dff26da7ba65786
Frank <frankxhunter@gmail.com>
2025-06-02 00:04:18 +0200

    Add CookieConfig to configure SameSite attribute for JSESSIONID cookies

---
+++ src/main/java/com/frank/shortify/configuration/CookieConfig.java
@@ -0,0 +1,14 @@
+package com.frank.shortify.configuration;
+
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CookieConfig {
+
+    @Bean
+    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone().whenHasName("JSESSIONID");
+    }
+}
