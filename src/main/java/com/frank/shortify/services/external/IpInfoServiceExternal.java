c1d076ca2d0c34a9fa530ffd0e4f7a1ae3e4dec3
frankxhunter <frankxhunter@gmail.com>
2026-06-06 14:03:56 +0200

    Add IpInfoServiceExternal control error

--- src/main/java/com/frank/shortify/services/external/IpInfoServiceExternal.java
+++ src/main/java/com/frank/shortify/services/external/IpInfoServiceExternal.java
@@ -3,6 +3,7 @@ package com.frank.shortify.services.external;
 import com.frank.shortify.dto.IpDataExternal;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -21,11 +22,22 @@ public class IpInfoServiceExternal {
     public IpDataExternal getInfoIp(String ip) {
         IpDataExternal result = restClient.get()
                 .uri("/{ip}", ip)
-                .retrieve().body(IpDataExternal.class);
-        if (StringUtils.isBlank(result.getCountryCode())) {
-            result.setCountry("Unknown");
-            result.setCountryCode("--");
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, (req, res) -> {
+                })
+                .body(IpDataExternal.class);
+
+        if (result == null || StringUtils.isBlank(result.getCountryCode())) {
+            return unknownIp();
         }
+
         return result;
+    }
+
+    private IpDataExternal unknownIp() {
+        IpDataExternal unknown = new IpDataExternal();
+        unknown.setCountry("Unknown");
+        unknown.setCountryCode("--");
+        return unknown;
     }
 }
